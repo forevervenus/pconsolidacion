411b0f08e5eaee8a551cb99d2bcb62044b4c2bb7
Fernanda <fcsandovalc@gmail.com>
2024-09-28 01:55:06 -0300

    Add files via upload

---
+++ pConsolidacion3.py
@@ -0,0 +1,18 @@
+
+
+magos = ['Harry Houdini', 'David Blaine', 'Teller']
+cientificos = ['Newton', 'Hawking', 'Einstein']
+otros = ['Messi', 'Pele', 'Juanes']
+nombres = magos + cientificos + otros
+def hacer_grandioso(magos):
+    for mago in magos:
+        return f'El gran: {mago}'
+
+
+
+
+#def imprimir_nombres():
+
+
+print(mago)
+print(nombres)
